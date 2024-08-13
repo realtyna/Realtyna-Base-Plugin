7d9dc9869193fbf063688bc54423a0a2875ec1bd
Chandler P <chandler.p@realtyna.net>
2024-08-13 11:29:57 -0400

    add basic Main class

---
+++ src/Main.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Realtyna\BasePlugin;
+
+use Realtyna\Core\StartUp;
+
+class Main extends StartUp
+{
+
+
+    protected function components()
+    {
+        // TODO: Implement components() method.
+    }
+
+    protected function setting()
+    {
+        // TODO: Implement setting() method.
+    }
+}
