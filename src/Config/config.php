a52f4cc9cb327c3e1ce32a3675ff497e35bf97a0
Chandler P <chandler.p@realtyna.net>
2024-08-13 11:29:42 -0400

    add basic config

---
+++ src/Config/config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * App configuration file.
+ */
+return [
+    'name' => 'Realtyna Base Plugin',
+    'slug' => 'realtyna-base-plugin',
+    'text-domain' => 'realtyna-base-plugin',
+    'log' => [
+        'active' => true,
+        'level' => 'error'
+    ],
+];
