385e0ff5bae4238e8c3732d2f0c9366c2cab03fe
Chandler P <chandler.p@realtyna.net>
2024-08-13 11:47:15 -0400

    Add Basic Container

--- app/StartUp.php
+++ app/StartUp.php
@@ -1,26 +1,58 @@
 <?php
+
 namespace Realtyna\Core;
 
-use Realtyna\Core\Interfaces\ComponentInterface;
+use Realtyna\Core\Abstracts\ComponentAbstract;
+use Realtyna\Core\Utilities\Container;
 
 defined('ABSPATH') || exit('No direct script access allowed');
+
 abstract class StartUp
 {
     private Config $config;
-    private array $components;
+    protected Container $container;
+    private array $components = [];
 
     public function __construct(Config $config)
     {
         $this->config = $config;
+        $this->container = new Container();
+        $this->components(); // Call to bind components
+        $this->registerComponents(); // Register all bound components
     }
 
-    public function addComponent(ComponentInterface $component): void
+    /**
+     * Adds a component class to the list of components to be registered.
+     *
+     * @param string $component
+     */
+    public function addComponent(string $component): void
     {
         $this->components[] = $component;
     }
 
+    /**
+     * Registers all components that were added using the addComponent method.
+     */
+    private function registerComponents(): void
+    {
+        foreach ($this->components as $component) {
+            $service = $this->container->get($component);
+            if ($service instanceof ComponentAbstract && method_exists($service, 'register')) {
+                $service->register();
+            }
+        }
+    }
 
-    abstract protected function components();
-    abstract protected function setting();
+    /**
+     * Abstract method to bind components to the container.
+     * This method must be implemented by the extending class.
+     */
+    abstract protected function components(): void;
 
+    /**
+     * Abstract method to handle plugin settings.
+     * This method must be implemented by the extending class.
+     */
+    abstract protected function setting(): void;
 }
