e577807f8bdc383e4fd50ad6b30d5a83db207233
Chandler P <chandler.p@realtyna.net>
2024-08-13 17:11:52 -0400

    Add Support for SubComponents in the ComponentAbstract.php

--- app/Abstracts/ComponentAbstract.php
+++ app/Abstracts/ComponentAbstract.php
@@ -11,6 +11,11 @@ namespace Realtyna\Core\Abstracts;
 abstract class ComponentAbstract
 {
     /**
+     * @var array List of subcomponent class names.
+     */
+    protected array $subComponents = [];
+
+    /**
      * @var array List of custom post type class names.
      */
     protected array $postTypes = [];
@@ -26,7 +31,9 @@ abstract class ComponentAbstract
     public function __construct()
     {
         $this->postTypes();
+        $this->subComponents();
         $this->registerPostTypes();
+        $this->registerSubComponents();
         $this->register();
     }
 
@@ -45,6 +52,14 @@ abstract class ComponentAbstract
      * @return void
      */
     abstract public function postTypes(): void;
+
+    /**
+     * Defines the subcomponents that the component should register.
+     * This method must be implemented by the subclass.
+     *
+     * @return void
+     */
+    abstract public function subComponents(): void;
 
     /**
      * Adds a custom post type to the component.
@@ -67,6 +82,33 @@ abstract class ComponentAbstract
         foreach ($this->postTypes as $postType) {
             $service = new $postType();
             add_action('after_setup_theme', [$service, 'register']);
+        }
+    }
+
+    /**
+     * Adds a subcomponent to the component.
+     *
+     * @param string $subComponent The class name of the subcomponent to add.
+     * @return void
+     */
+    public function addSubComponent(string $subComponent): void
+    {
+        $this->subComponents[] = $subComponent;
+    }
+
+    /**
+     * Registers all subcomponents associated with the component.
+     *
+     * @return void
+     * @throws \ReflectionException
+     */
+    private function registerSubComponents(): void
+    {
+        foreach ($this->subComponents as $subComponent) {
+            $service = new $subComponent();
+            if ($service instanceof ComponentAbstract && method_exists($service, 'register')) {
+                $service->register();
+            }
         }
     }
 }
