c44dbf914e50a2f7f34ae285ea171052d5ddbb27
Chandler P <chandler.p@realtyna.net>
2024-08-13 13:28:07 -0400

    Add methods to register AdminPage and AdminPageAbstract.php

--- app/StartUp.php
+++ app/StartUp.php
@@ -2,6 +2,7 @@
 
 namespace Realtyna\Core;
 
+use Realtyna\Core\Abstracts\AdminPageAbstract;
 use Realtyna\Core\Abstracts\ComponentAbstract;
 use Realtyna\Core\Utilities\App;
 use Realtyna\Core\Utilities\Container;
@@ -11,6 +12,7 @@ abstract class StartUp
     private Config $config;
     protected Container $container;
     private array $components = [];
+    private array $adminPages = [];
 
     public function __construct(Config $config)
     {
@@ -18,6 +20,8 @@ abstract class StartUp
         $this->container = new Container();
 
         $this->boot();
+        $this->setting();
+        $this->registerSettings();
 
         $this->components();
         $this->registerComponents();
@@ -33,6 +37,24 @@ abstract class StartUp
         foreach ($this->components as $component) {
             $service = $this->container->get($component);
             if ($service instanceof ComponentAbstract && method_exists($service, 'register')) {
+                $service->register();
+            }
+        }
+    }
+
+    public function addAdminPage(string $adminPage): void
+    {
+        $this->adminPages[] = $adminPage;
+    }
+
+    /**
+     * @throws \ReflectionException
+     */
+    private function registerSettings(): void
+    {
+        foreach ($this->adminPages as $adminPage) {
+            $service = $this->container->get($adminPage);
+            if ($service instanceof AdminPageAbstract && method_exists($service, 'register')) {
                 $service->register();
             }
         }
